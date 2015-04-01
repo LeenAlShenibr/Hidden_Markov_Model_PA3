ff989fdb7c91cc11b66341d3650aa90251428429
LeenAlShenibr <Distraught63@gmail.com>
2015-04-01 13:46:07 -0400

    Updated reading method to work with java6, and finished viterbi algorithm

--- HMM.java
+++ HMM.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 
 class HMM
 {
@@ -26,9 +27,30 @@ class HMM
     B = new double[N][M];
   }
 
+  public int[] translateObservations (String[] obs)
+  {
+    int N = obs.length;
+    int[] obsIndexes = new int[N];
+
+    for(int i = 0; i < N; i++)
+    {
+      for(int j = 0; j < outputs.length; j++)
+      {
+        if (obs[i].equals(outputs[j]))
+        {
+          obsIndexes[i] = j;
+          break;
+        }
+      }
+    }
+
+    return obsIndexes;
+
+  }
+
   public void outputHMM(PrintWriter out)
   {
-    
+
     out.printf("%d %d %d\n", states.length, outputs.length, T);
 
     //States
@@ -75,6 +97,122 @@ class HMM
     }
     out.println();
 
+
+  }
+
+
+  public double statepath(String[] observationStrings)
+  {
+
+    //==========================================================================
+    //======== Initilization
+    //==========================================================================
+
+
+    int N = pi.length;
+    int T2 = observationStrings.length;
+    int [] observations  = translateObservations(observationStrings);
+    int[][] paths= new int [T][N];
+    double[][] bestScores = new double [T][N];
+
+    //Initilization step
+    for (int n = 0; n < N; n++)
+    {
+      bestScores[0][n] = pi[n] * B[n][observations[0]];
+      paths[0][n] = 0;
+    }
+
+    //==========================================================================
+    //======== Recursion
+    //==========================================================================
+
+    double max;
+    int argMax;
+    double score;
+
+    for (int t = 1; t < T2; t++)
+    {
+
+      for(int j = 0; j < N; j++)
+      {
+
+        max = 0;
+        argMax = 0;
+
+        for(int i = 0; i < N; i++)
+        {
+          score = bestScores[t - 1][i] * A[i][j];
+
+          if (score > max)
+          {
+            max = score;
+            argMax = i;
+          }
+
+        }
+
+        bestScores[t][j] = max * B[j][observations[t]];
+        paths[t][j] =  argMax;
+
+      }
+
+    }
+
+    //==========================================================================
+    //======== Termination
+    //==========================================================================
+
+    max = 0;
+    argMax= 0;
+
+    int[] path = new int[T2];
+
+    for(int i = 0; i < N; i++)
+    {
+      score = bestScores[T2-1][i];
+
+      if ( score > max )
+      {
+        max = score;
+        argMax = i;
+      }
+
+    }
+
+    double probT = max;
+    path[T2-1] = argMax;
+
+    //==========================================================================
+    //======== Path Backtracking
+    //==========================================================================
+
+    for(int t = T2-2; t >= 0; t--)
+    {
+      path[t] = paths[t+1][path[t+1]];
+    }
+
+    //==========================================================================
+    //======== Print Result
+    //==========================================================================
+
+    System.out.print(probT);
+
+    if(probT > 0)
+    {
+
+      for(int t = 0; t < T2; t++)
+      {
+        System.out.printf(" %s", states[path[t]]);
+      }
+
+    }
+    
+    System.out.println();
+
+
+
+
+    return 0;
 
   }
 
--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Scanner;
 import java.io.*;
 
 class Main
@@ -12,7 +11,7 @@ class Main
 
 
   //==========================================================================
-  //======== Test Data
+  //======== Processing
   //==========================================================================
 
 
@@ -21,23 +20,25 @@ class Main
 
     try
     {
-      List<String> lines = Files.readAllLines(Paths.get(path));
+
+      String output = new Scanner(new File(path)).useDelimiter("\\Z").next();
+      String[] lines = output.split("\n");
 
 
 
       //Get N, M, T and init HMM
-      String[] splitLine = lines.get(0).split(" ");
+      String[] splitLine = lines[0].split(" ");
       hmm.initHMM(Integer.parseInt(splitLine[0]), Integer.parseInt(splitLine[1]), Integer.parseInt(splitLine[2]));
 
       //Get and Set State strings
-      String[] states = lines.get(1).split(" ");
+      String[] states = lines[1].split(" ");
       for (int i = 0; i < states.length; i++)
       {
         hmm.states[i] = states[i];
       }
 
       //Get and set Output strings
-      String[] outputs = lines.get(2).split(" ");
+      String[] outputs = lines[2].split(" ");
       for (int i = 0; i < outputs.length; i++)
       {
         hmm.outputs[i] = outputs[i];
@@ -46,7 +47,7 @@ class Main
       //Get and Set A
       for(int i = 4; i < 8; i++)
       {
-        String[] line = lines.get(i).split(" ");
+        String[] line = lines[i].split(" ");
 
         for(int j = 0; j < line.length; j++)
         {
@@ -58,7 +59,7 @@ class Main
       //Get and Set B
       for(int i = 9; i < 13; i++)
       {
-        String[] line = lines.get(i).split(" ");
+        String[] line = lines[i].split(" ");
 
         for(int j = 0; j < line.length; j++)
         {
@@ -69,7 +70,7 @@ class Main
 
 
       //Get and Set Pi
-      String[] initial = lines.get(14).split(" ");
+      String[] initial = lines[14].split(" ");
       for(int i = 0; i < initial.length; i++)
       {
 
@@ -88,18 +89,21 @@ class Main
   {
     try
     {
-      List<String> lines = Files.readAllLines(Paths.get(path));
+      String output = new Scanner(new File(path)).useDelimiter("\\Z").next();
+      String[] lines = output.split("\\r?\\n");
 
-      int n = Integer.parseInt(lines.get(0));
+
+      int n = Integer.parseInt(lines[0]);
       observations = new String[n][n];
 
       int index = 0;
-      for(int i = 1; i < lines.size(); i+=2)
+      for(int i = 1; i < lines.length; i+=2)
       {
 
-        observations[index] = new String [Integer.parseInt(lines.get(i))];
+        //Number of observations
+        observations[index] = new String [Integer.parseInt(lines[i])];
 
-        String[] splitLine = lines.get(i+1).split(" ");
+        String[] splitLine = lines[i+1].split(" ");
 
         for(int j = 0; j < splitLine.length; j++)
         {
@@ -121,16 +125,22 @@ class Main
 
   }
 
+
   private static void initWriter(String path)
   {
     File file = new File(path);
     try {
       out = new PrintWriter(file);
     } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
   }
+
+
+  //==========================================================================
+  //======== Main and Debugging methdos
+  //==========================================================================
+
 
 
 
@@ -181,7 +191,10 @@ class Main
     }
     else if (hmmMethod.equals("statepath"))
     {
-
+      for(int i = 0; i < observations.length; i++)
+      {
+        hmm.statepath(observations[i]);
+      }
     }
     else if (hmmMethod.equals("recognize"))
     {
